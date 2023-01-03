af6830b8e97e5247d9627ff4bd325e50565d5f58
devYuMinKim <gimyumin40@gmail.com>
2023-01-03 20:49:44 +0900

    Refactor : 레포지토리 정리

--- src/literal_type.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Literal Type
- */
-
-let letString = 'Hello'       // string
-const constString = 'Hello'   // "Hello"
-
-let letNumber = 123
-const constNumber = 123
--- src/static_typing.ts
+++
@@ -1,7 +0,0 @@
-let foo: string,  // 문자열 타입
-		bar: number,  // 숫자 타입
-		baz: boolean; // 불리언 타입
-
-foo = 'Hello';
-bar = 123;
-// baz = 'true'; // 에러 발생. boolean 값이 아닌 String 타입이기 때문.
